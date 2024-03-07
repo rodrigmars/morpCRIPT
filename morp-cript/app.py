57e5c7371bb77def9bbc99d97af46091788a3053
rodrigmars <rodrigmars@gmail.com>
2024-03-07 11:44:32 -0300

    Função create_bag criada

--- morp-cript/app.py
+++ morp-cript/app.py
@@ -1,32 +1,55 @@
 def main():
 
-    def build_frase(frase):
+    def pick_up_words(frase):
+
+        vowels = ['a', 'e', 'i', 'o', 'u']
 
         punctuation = ['.', ',', ';', ':', '!',
                        '?', '...', '“', '”', '(', ')', '—']
 
         for p in punctuation:
-            frase = frase.replace(p, "\n")
+            frase = frase.replace(p, '\u0020')
 
-        def remove_space():
-            return frase.split()
+        def create_bag():
 
-        return remove_space
+            words = []
 
-    words = []
+            for w in frase.split():
 
-    vowels = ['a', 'e', 'i', 'o', 'u']
+                # if w in vowels:
+                #     continue
 
-    morpheus = "Cedo ou tarde você vai aprender, assim como eu aprendi, que há uma diferença entre conhecer o caminho e percorrer o caminho"
+                words.append(w)
 
-    for word in build_frase(morpheus)():
+            def Counter():
 
-        if word in vowels:
-            continue
+                def count_words(unique_word):
 
-        words.append(word)
+                    words_dict = {}
+                    count = 0
 
-    print(words)
+                    for w in words:
+
+                        if unique_word.lower() == w.lower():
+
+                            count += 1
+                            words_dict[unique_word] = count
+
+                    return words_dict
+
+                return list(map(count_words, set(words)))
+
+            return Counter
+
+        return create_bag
+
+    # frase = "Cedo ou tarde.de manhã,,,você.vai aprender, assim como eu aprendi, que há uma diferença entre conhecer o caminho e percorrer o caminho"
+    frase = "areia areia areia no no olho do do do do furacão Em meio milhões"
+
+    words = pick_up_words(frase)()()
+
+    for w in words:
+        print(w)
 
 
 if __name__ == "__main__":
