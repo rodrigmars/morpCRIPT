2dc75191eabf29645103fd8c9045886ba7de4435
rodrigmars <rodrigmars@gmail.com>
2024-03-08 05:59:41 -0300

    Retirada da função create_bag

--- morp-cript/app.py
+++ morp-cript/app.py
@@ -14,45 +14,41 @@ def main():
         for p in punctuation:
             frase = frase.replace(p, '\u0020')
 
-        def create_bag():
+        words = frase.split()
 
-            words = frase.split()
+        def get_occurrences():
 
-            def get_occurrences():
+            def count_words(unique_word):
 
-                def count_words(unique_word):
+                count = 0
 
-                    count = 0
+                words_dict = {}
 
-                    words_dict = {}
+                for w in words:
 
-                    for w in words:
+                    if unique_word.lower() == w.lower():
 
-                        if unique_word.lower() == w.lower():
+                        count += 1
 
-                            count += 1
+                        words_dict[w] = count
 
-                            words_dict[w] = count
+                return words_dict
 
-                    return words_dict
+            occurrences = list(map(count_words, set(words)))
 
-                occurrences = list(map(count_words, set(words)))
+            word_dictionary = []
 
-                word_dictionary = []
+            for score in occurrences:
 
-                for score in occurrences:
+                for k, v in score.items():
+                    word_dictionary.append({"word": k,
+                                            "total": v})
 
-                    for k, v in score.items():
-                        word_dictionary.append({"word": k,
-                                                "total": v})
+            return sorted(word_dictionary,
+                          key=lambda x: x["total"],
+                          reverse=True)
 
-                return sorted(word_dictionary,
-                              key=lambda x: x["total"],
-                              reverse=True)
-
-            return get_occurrences
-
-        return create_bag
+        return get_occurrences
 
     # frase = "Cedo ou tarde.de manhã,,,você.vai aprender, assim como eu aprendi, que há uma diferença entre conhecer o caminho e percorrer o caminho"
 
@@ -62,7 +58,7 @@ def main():
 
     try:
 
-        for occurrences in text_mining(frase)()():
+        for occurrences in text_mining(frase)():
 
             print(f"{occurrences['word']}: {occurrences['total']}")
 
