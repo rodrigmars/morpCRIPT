b53bedc952ca69aa0d78799b001bcae271579b73
rodrigmars <rodrigmars@gmail.com>
2024-03-08 08:27:27 -0300

    Otimização executada, código alterado

--- morp-cript/app.py
+++ morp-cript/app.py
@@ -6,53 +6,26 @@ def main():
 
     def text_mining(frase):
 
-        # vowels = ['a', 'e', 'i', 'o', 'u']
-
         punctuation = ['.', ',', ';', ':', '!',
                        '?', '...', '“', '”', '(', ')', '—']
 
         for p in punctuation:
             frase = frase.replace(p, '\u0020')
 
-        words = frase.split()
-
         def get_occurrences():
 
-            def count_words(unique_word):
+            occurrences = list(map(lambda x: {"word": x, "total": frase.split().count(x)},
+                                   set(frase.split())))
 
-                count = 0
-
-                words_dict = {}
-
-                for w in words:
-
-                    if unique_word.lower() == w.lower():
-
-                        count += 1
-
-                        words_dict[w] = count
-
-                return words_dict
-
-            occurrences = list(map(count_words, set(words)))
-
-            word_dictionary = []
-
-            for score in occurrences:
-
-                for k, v in score.items():
-                    word_dictionary.append({"word": k,
-                                            "total": v})
-
-            return sorted(word_dictionary,
-                          key=lambda x: x["total"],
-                          reverse=True)
+            return sorted(occurrences, key=lambda x: x["total"], reverse=True)
 
         return get_occurrences
 
     # frase = "Cedo ou tarde.de manhã,,,você.vai aprender, assim como eu aprendi, que há uma diferença entre conhecer o caminho e percorrer o caminho"
 
-    frase = "areia areia areia no no olho do do do do furacão Em meio milhões"
+    # frase = "areia areia areia no no olho do do do do furacão Em meio milhões"
+
+    frase = "porta quadro mesa janela mesa poltrona janela casa cadeira mesa sofá porta janela parede mesa sofá quadro"
 
     # frase = "c d f A A e b B i o u h t v z A f x h A"
 
